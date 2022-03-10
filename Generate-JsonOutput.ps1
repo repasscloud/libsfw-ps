a0be1d366511b839ac467c7ddf6d9c159cc128f5
danijeljw-rpc <danijel@repasscloud.com>
2022-03-11 10:30:32 +1100

    template new function

---
+++ Generate-JsonOutput.ps1
@@ -0,0 +1,101 @@
+function Export-JsonOutput {
+    [CmdletBinding()]
+    param (
+        [System.String]$NuspecUri,
+        [System.String]$Homepage,
+        [System.String]$IconUri,
+        [System.String]$Copyright,
+        [System.String]$License,
+        [System.String]$Docs,
+        [System.String]$Tags,
+        [System.String]$Summary,
+        [ValidateSet("True","False")]
+        [System.String]$RebootRequired,
+        [System.String]$Depends,
+        [ValidateSet("Productivity")]
+        [System.String]$Category,
+        [ValidateSet("mc")]
+        [System.String]$XFT,
+        [ValidateSet("au-syd1-07")]
+        [System.String]$Locale,
+        [Required]
+        [System.String]$Version,
+        [System.String]$Name,
+        [System.String]$Publisher,
+        [System.String]$Arch,
+        [System.String]$LCID,
+        [System.String]$UID,
+        [ValidateSet("Exe","Msi")]
+        [System.String]$Type,
+        [System.String]$FileName,
+        [System.String]$SHA256,
+        [System.String]$FollowUri,
+        [System.String]$Switches,
+        [System.String]$DisplayName,
+        [System.String]$DisplayVersion,
+        [System.String]$DisplayPublisher,
+        [System.String]$Path,
+        [ValidateSet("au-syd1-07")]
+        [System.String]$Geo,
+        [System.String]$UninstallString,
+        [System.String]$UninstallArgs,
+        [System.String]$SysInfo="4.4.0.1"
+
+    )
+    
+    begin {
+        <# PRELOAD - DO NOT EDIT #>
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+        $userAgent = [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer
+
+        <# JSON DATA STRUCTURE - DO NOT EDIT #>
+        $JsonDict = [System.Collections.Specialized.OrderedDictionary]@{}
+        $JsonDict.meta = [System.Collections.Specialized.OrderedDictionary]@{}
+        $JsonDict.id = [System.Collections.Specialized.OrderedDictionary]@{}
+        $JsonDict.installer = [System.Collections.Specialized.OrderedDictionary]@{}
+        $dJsonDict.uninstaller = [System.Collections.Specialized.OrderedDictionary]@{}
+        $JsonDict.sysinfo = [System.Collections.Specialized.OrderedDictionary]@{}
+    }
+    
+    process {
+        # download Nuspec file
+        if ($null -notlike $NuspecUri)
+        {
+            if (Test-Path -Path "$($env:TMP)\nuspec.xml") { Remove-Item -Path "$($env:TMP)\nuspec.xml" -Confirm:$false -Force }
+            $wc = New-Object System.Net.WebClient
+            $wc.Headers.Add("user-agent", $userAgent)
+            try
+            {
+                $wc.DownloadFile($NuspecUri, "$($env:TMP)\nuspec.xml")
+                $wc.Dispose()
+                [xml]$XmlNuspec = Get-Content -Path "$($env:TMP)\nuspec.xml"
+                $AppVersionFromRegex = $XmlNuspec.package.metadata.version
+                Remove-Item -Path "$($env:TMP)\nuspec.xml" -Confirm:$false -Force
+            }
+            catch
+            {
+                Write-Output "Version info cannot be confirmed"
+            }
+        }
+
+        if ($null -notlike $Version)
+        {
+            switch ($Version -like $AppVersionFromRegex)
+            {
+                $True {
+                    # Nuspec file match
+                    $JsonDict.id.version = $AppVersionFromRegex
+                }
+                Default {
+                    # Nuspec file did not match
+                    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) NUSPEC DID NOT MATCH VERSION PROVIDED"
+                    $JsonDict.id.version = $Version
+                }
+            }
+        }
+    }
+    
+    end {
+        
+    }
+}
