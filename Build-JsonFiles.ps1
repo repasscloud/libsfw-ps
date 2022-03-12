fad08b32fc97dbb2cfad9b0f8cb94ed13aafe6ce
danijeljw-rpc <danijel@repasscloud.com>
2022-03-13 01:27:12 +1100

    using Build-JsonFiles function

--- Build-JsonFiles.ps1
+++ Build-JsonFiles.ps1
@@ -11,22 +11,18 @@ function Build-JsonFiles {
     
     foreach ($ps1 in $PS1SrcFiles)
     {
-        # set name and full name
-        [System.String]$jsonFileSrc = $ps1.Name
-        [System.String]$jsonPathSrc = $ps1.FullName
-
         # execute the generation of the JSON library file
         try
         {
             # build the file
-            & $jsonPathSrc
+            & $ps1.FullName
 
             # advise the file has been built
-            Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) BUILT JSON FILE FOR INGEST: ${jsonFileSrc}"
+            Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) BUILT JSON FILE FOR INGEST: $($ps1.Name)"
         }
         catch
         {
-            Write-Output "$([System.Char]::ConvertFromUTF32("0x1F534")) UNABLE TO PROCESS: ${jsonFileSrc}"
+            Write-Output "$([System.Char]::ConvertFromUTF32("0x1F534")) UNABLE TO PROCESS: $($ps1.Name)"
         }
     }
 }
